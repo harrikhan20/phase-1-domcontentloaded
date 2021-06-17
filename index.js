d9a18ffee140ba964f81173286db671be26ec5fa
harris <khanharris7@yahoo.com>
2021-06-16 19:59:10 -0500

    commit tree a738b8a56020a7d057e5f3074e474152155533ad

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Your code goes here
+
+//document.addEventListener("DOMContentLoaded", function() {
+   // console.log("This is really cool!");
+  // const text = document.getElementById("text")
+  // console.log("This is really cool!")
+ // });
+  
+  //console.log('This is really cool!')
+  //  "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+  //; 
+
+
+  //const p2 = document.querySelector('p');
+  //p2.innerHTML.replace("This is really cool!");
+
+  
+
+  document.addEventListener("DOMContentLoaded", function() {
+  const ptag = document.querySelector('#text')
+  const newtext = "This is really cool!";
+  ptag.innerHTML = newtext;
+  });
+  
+  
